64d178d4ffc4467544dcfbe2ea4f54f4ef63ed0b
Mstars_lab1_07 <mstars_lab1_07@Mstars-lab1-07s-iMac.local>
2022-04-07 10:34:37 +0800

    cors shiidegdsen baigasai gj naidaj bna

--- back-end/index.js
+++ back-end/index.js
@@ -9,23 +9,17 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use('/', apiRoutes);
 app.use(cors());
+app.use((req, res) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+});
 
 app.all('*', (req, res, next) => {
     res.status(404).json({
         success: false,
         message: `Can't find ${req.originalUrl} on this server`
     })
-});
-
-const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200,
-}
-
-app.all('*', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    next();
 });
 
 const port = 3002;
--- back-end/routes/api.js
+++ back-end/routes/api.js
@@ -5,7 +5,7 @@ const LibraryController = require('../controller/LibraryController');
 // Requests
 router.get('/books', LibraryController.get_books);
 router.post('/addBook', LibraryController.add_book);
-router.put('/updateBook/:isbn', LibraryController.update_book);
-router.delete('/deleteBook/:isbn', LibraryController.delete_books);
+router.put('/updateBook/:id', LibraryController.update_book);
+router.delete('/deleteBook/:id', LibraryController.delete_books);
 
 module.exports = router;
