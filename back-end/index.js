8d2841ba8851a85e2f9fdbbee2d74c69b830e31e
Mstars_lab1_07 <mstars_lab1_07@Mstars-lab1-07s-iMac.local>
2022-04-07 12:42:14 +0800

    nuguu l neg cors iin asudal

--- back-end/index.js
+++ back-end/index.js
@@ -18,6 +18,7 @@ app.all('*', (req, res, next) => {
 });
 
 app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Credentials: true");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST, HEAD, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
