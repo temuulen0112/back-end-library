dcc12cc51bb9754947e38e71b4b9b0723e09ea0a
Mstars_lab1_07 <mstars_lab1_07@Mstars-lab1-07s-iMac.local>
2022-04-11 09:55:31 +0800

    tiimeee cors iig zaschlaa

--- back-end/index.js
+++ back-end/index.js
@@ -8,18 +8,10 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/', apiRoutes);
 app.use(cors({
     origin: '*'
 }));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.all('*', (req, res, next) => {
-    res.status(404).json({
-        success: false,
-        message: `Can't find ${req.originalUrl} on this server`
-    })
-});
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -28,6 +20,17 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use('/', apiRoutes);
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: `Can't find ${req.originalUrl} on this server`
+    })
+});
+
+
 
 const port = 3002;
 app.listen(port, () => {
